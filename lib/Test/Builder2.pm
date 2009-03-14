b58a808d585362958a1963976333ee9ca270730f
notbenh <benh@lappy.local>
2009-03-14 12:16:50 -0700

    more playing, its kinda outputting tap now... kinda

--- lib/Test/Builder2.pm
+++ lib/Test/Builder2.pm
@@ -2,17 +2,37 @@ package Test::Builder2;
 use Moose;
 use Test::Builder2::Result;
 
+has output => (
+   is => 'rw',
+   isa => 'Test::Builder2::Output',
+   default => sub{
+      use Test::Builder2::Output::Tap;
+      Test::Builder2::Output::Tap->new;
+   },
+);
+
+has i => (
+   is => 'rw',
+   isa => 'Int',
+   default => 0,
+);
+
 sub ok {
    my ($self, $test) = @_;
-   my $res = Test::Builder2::Result->new;
+   my $res = Test::Builder2::Result->new(output => $self->output);
    $res->result( ($test) ? 1 : 0 );
    return $res;
 }
 
 sub diag {
    my ($result, $msg) = @_;
-   warn $msg
-      unless $result->result;
+   $result->reason($msg);
+   return $result;
+};
+
+sub diag_append {
+   my ($result, $msg) = @_;
+   $result->reason( $result->reason .' '. $msg);
    return $result;
 };
 
--- lib/Test/Builder2/Result.pm
+++ lib/Test/Builder2/Result.pm
@@ -15,16 +15,48 @@ has name => (
    default => '',
 );
 
-has expected => (
+has reason => (
+   is => 'rw',
+   isa => 'Str',
+   default => '',
+);
+
+has _expected => (
    is => 'rw',
    isa => 'Defined',
    predicate => 'has_expected',
 );
+sub expected {
+   my ($self,$expected) = @_;
+   $self->_expected($expected);
+   return $self;
+}
 
-has recieved => (
+has _recieved => (
    is => 'rw',
    isa => 'Defined',
    predicate => 'has_recieved',
 );
+
+sub recieved {
+   my ($self, $recieved) = @_;
+   $self->recieved($recieved);
+   return $self;
+}
+
+sub and {
+   my ($self) = @_;
+   return $self
+      if $self->result;
+   $self->done(1);
+   return undef;
+}
+
+
+sub DEMOLISH {
+   my $self = shift;
+   $self->output->display($self);
+}
+
 
 1;
